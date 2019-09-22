a75a3b3a1cc59f929ef3d39c04b4f7d4333358ea
Anton Makhankov <36548646+vvayffaring@users.noreply.github.com>
2019-09-23 00:36:23 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<link rel="stylesheet" href="css/popup.css" type="text/css" />
+<link rel="stylesheet" href="css/media-queries.css" type="text/css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<title>Великий Новгород</title>
+</head>
+
+<body>
+
+<div class="pagewrap">
+  
+  <div class="header"> 
+   <div class="container">
+    <h2>Великий Новгород</h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Left column LOGO</div>
+        <div class="col-md-4 col-sm-6 col-xs-12">Center column MENU</div>
+        <div class="col-md-4 col-sm-6 col-xs-12">Right column PHONE</div> 
+     </div> 
+   </div>
+  </div>
+
+  <div class="main_slider"> 
+   <div class="container">
+    <h2>Диагностика в ЦМРТ</h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Left column SLIDER</div>
+        <div class="col-md-4 col-sm-6 col-xs-12">Right column PHOTO</div> 
+     </div> 
+   </div>
+  </div>
+
+  <div class="maini_slider"> 
+   <div class="container">
+    <h2>Наши направления</h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Маленький слайдер</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="uslugi"> 
+   <div class="container">
+    <h2>Услуги</h2>
+       <div class="row"> 
+          <div class="col-md-4 col-sm-4 col-xs-12">Left column USLUGI</div>
+          <div class="col-md-8 col-sm-8 col-xs-12">
+            <div class="search">Поиск</div>
+            <div class="container"> 
+              <div class="row"> 
+                <div class="col-md-4 col-sm-6 col-xs-12">Консультации</div>
+                <div class="col-md-4 col-sm-6 col-xs-12">Лечение</div> 
+              </div> 
+            </div>
+          </div> 
+       </div> 
+   </div>
+  </div>
+
+  <div class="diagnostika">
+    <div class="container">
+      <h2>МРТ-диагностика</h2>
+         <div class="row"> 
+            <div class="col">4 Фото Диагностики</div>
+            <div class="col">МРТ-диагностика текстовый блок</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="utp">
+    <div class="container">
+      <h2></h2>
+         <div class="row"> 
+            <div class="col">Круглосуточно  и без выходных</div>
+            <div class="col">Стоимость МРТ от 1800 ₽</div>
+            <div class="col">Обследование в день обращения</div>
+            <div class="col">30% скидка на обследования ночью</div>
+         </div> 
+    </div>
+  </div>
+
+  <div class="utp-2">
+    <div class="container">
+      <h2></h2>
+         <div class="row"> 
+            <div class="col">Преимущества МРТ-диагностики</div>
+            <div class="col">GE Signa Echospeed Plus 1,5 Тл</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="recomendation">
+    <div class="container">
+      <h2>Рекомендации по подготовке к МРТ-диагностике</h2>
+         <div class="row"> 
+            <div class="col">1 2 обзац</div>
+            <div class="col">3 4 обзац</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="about_clinic">
+    <div class="container">
+      <h2>О клинике</h2>
+         <div class="row"> 
+            <div class="col">текстовый блок</div>
+            <div class="col">Подробнее о ЦМРТ</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="mишп_slider"> 
+   <div class="container">
+    <h2></h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Большой слайдер</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="license">
+    <div class="container">
+      <h2></h2>
+         <div class="row"> 
+            <div class="col">Фото лицензии</div>
+            <div class="col">УТП + текст</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="doctors"> 
+   <div class="container">
+    <h2>Врачи</h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Cлайдер с врачами</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="question-answer">
+    <div class="container">
+      <h2>Вопрос-ответ</h2>
+         <div class="row"> 
+            <div class="col">Все темы вопросов</div>
+            <div class="col">Все врачи</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="kartochka-slider">
+    <div class="container">
+      <h2></h2>
+         <div class="row"> 
+              <div class="col">
+                <div class="container">
+                 <div class="row"> 
+                        <div class="col">1</div>
+                        <div class="col">2</div>
+                        <div class="col">3</div>
+                        <div class="col">4</div> 
+                 </div> 
+                </div>
+              </div>
+            <div class="col">
+              <div class="container">
+                 <div class="row"> 
+                        <div class="col">5</div>
+                        <div class="col">6</div>
+                        <div class="col">7</div>
+                        <div class="col">8</div> 
+                 </div> 
+              </div>
+            </div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="button_sugnup"> 
+   <div class="container">
+    <h2></h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Записаться и получить ответы на приёме у специалиста</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="tax-deduction">
+    <div class="container">
+      <h2>Налоговый вычет</h2>
+         <div class="row"> 
+            <div class="col">
+              <p>Параграф 1</p>
+              <p>Параграф 2</p>
+                <div class="text_mini_images">
+                  <div class="container">
+                    <h2></h2>
+                       <div class="row">
+                          <div class="col">Баннер УТП</div>
+                          <div class="col">Текст</div>
+                          <div class="col">Картинка</div> 
+                       </div> 
+                  </div>
+                </div>
+            </div>
+            <div class="col">Все врачи</div> 
+         </div> 
+    </div>
+  </div>
+
+  <div class="otzivi">
+    <div class="container">
+      <div class="row"> 
+          <div class="col">1</div>
+          <div class="col">2</div>
+          <div class="col">3</div>
+          <div class="col">4</div> 
+      </div> 
+    </div>
+  </div>
+
+  <div class="blog"> 
+   <div class="container">
+    <h2>Блог</h2>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Слайдер с картинками</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="block-zapis"> 
+   <div class="container">
+    <h2>Записаться в клинику ЦМРТ</h2>
+    <p>Позвоните по телефону +7 495</p>
+     <div class="row"> 
+        <div class="col-md-4 col-sm-6 col-xs-12">Форма</div>
+     </div> 
+   </div>
+  </div>
+
+  <div class="footer">
+    <div class="container">
+      <div class="row"> 
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div> 
+      </div> 
+    </div>
+
+    <div class="container">
+      <div class="row"> 
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div>
+        <div class="col">5</div> 
+        <div class="col">6</div> 
+      </div> 
+    </div>
+
+    <div class="container">
+      <div class="row"> 
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div>
+        <div class="col">5</div> 
+      </div> 
+    </div>
+
+    <div class="container">
+      <div class="row"> 
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+        <div class="col">4</div>
+      </div> 
+    </div>
+</div>
+
+
+</div>
+
+
+    <!-- Подключаем jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <!-- Подключаем плагин Popper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    
+    <!-- Подключаем Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous" ></script>
+
+</body>
+</html>
